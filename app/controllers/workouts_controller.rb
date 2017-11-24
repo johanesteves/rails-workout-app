202a43fddb2c2a8679a4b82f85fede4eac955cd0
johanesteves <jsesteves91@gmail.com>
2017-11-24 13:40:29 -0500

    Added Edit, Show, Update & Create Workout routes

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,6 +2,39 @@ class WorkoutsController < ApplicationController
 
   def index
     @workouts = Workout.where(user_id: params[:user_id])
+    @workout = User.find(params[:user_id]).workouts.build
   end
 
+  def show
+    @workout = Workout.find_by(user_id: params[:user_id], id: params[:id])
+    @exercises = @workout.exercises
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to user_workout_path(@workout.user, @workout)
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @workout = Workout.find_by(user_id: params[:user_id], id: params[:id])
+  end
+
+  def update
+    @workout = Workout.find_by(user_id: params[:user_id], id: params[:id])
+    if @workout.update(workout_params)
+      redirect_to user_workout_path(@workout.user, @workout)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def workout_params
+    params.require(:workout).permit(:name, :date, :user_id)
+  end
 end
