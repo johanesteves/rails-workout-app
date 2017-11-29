314119c62d1daaae2b4771c4302ca098f2931a2c
johanesteves <jsesteves91@gmail.com>
2017-11-28 20:39:31 -0500

    Updated custom setter to avoid bad body parts

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -6,17 +6,13 @@ class Workout < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validate :date_cannot_be_in_the_future
 
-  scope :current_week, -> {where("date BETWEEN ? AND ?", Date.today.at_beginning_of_week, Date.today.at_end_of_week)}
+  scope :current_week, -> {where('date BETWEEN ? AND ?', Date.today.at_beginning_of_week, Date.today.at_end_of_week)}
 
   def exercises_attributes=(exercise_attributes)
     exercise_attributes.each_value do |exercise_params|
-      # exercise = Exercise.where(exercise_params).first_or_create
-      # if exercise.errors.full_messages.first
-      #   exercise.errors.full_messages.first
-      # else
-      #   exercises << exercise unless exercise_params.values.any? {|i| i.empty? }
-      # end
-      exercises << Exercise.where(exercise_params).first_or_create unless exercise_params.values.any? {|i| i.empty? }
+      unless exercise_params.values.any? {|i| i.empty? } || Exercise.body_parts.exclude?(exercise_params[:bodypart])
+        exercises << Exercise.find_or_create_by(exercise_params)
+      end
     end
   end
 
