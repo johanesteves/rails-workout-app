79902848b6ec889a7e144123152d23db16801c2b
johanesteves <jsesteves91@gmail.com>
2017-11-30 23:07:50 -0500

    Added reps column to ExerciseWorkout jointable

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126181848) do
+ActiveRecord::Schema.define(version: 20171130223709) do
 
   create_table "exercise_workouts", force: :cascade do |t|
     t.integer "workout_id"
     t.integer "exercise_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reps"
   end
 
   create_table "exercises", force: :cascade do |t|
